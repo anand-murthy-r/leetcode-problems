a96390c897cd8c9c39043c1868bd9f46ea6fbf3e
anand-murthy-r <anu.2004.mys@gmail.com>
2025-10-21 11:58:00 +0530

    union find

--- 323_connected_components_undirected_graph.cpp
+++ 323_connected_components_undirected_graph.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-class Solution {
+class DFSApproach {
 private:
     unordered_map<int, vector<int>> graph;
 public:
@@ -43,10 +43,52 @@ public:
 
 };
 
+class UFApproach {
+    private:
+        int find(const int& node, vector<int>& parent) {
+            int res = node;
+            while (res != parent[res]) {
+                parent[res] = parent[parent[res]];
+                res = parent[res];
+            }
+            return res;
+        }
+
+        int Union(const int& node1, const int& node2, vector<int>& parent, vector<int>& rank) {
+            int root1 = find(node1, parent);
+            int root2 = find(node2, parent);
+            if (root1 == root2) return 0;
+
+            if (rank[root1] > rank[root2]) {
+                parent[root2] = root1;
+                rank[root1] += rank[root2];
+            }
+            else {
+                parent[root1] = root2;
+                rank[root2] += rank[root1];
+            }
+            return 1;
+        }
+    public:
+        int countComponents(int n, vector<vector<int>>& edges) {
+            vector<int> parent(n), rank(n, 1);
+            int result = n;
+            for(int i = 0; i < n; i++) parent[i] = i;
+
+            for(vector<int> p : edges) {
+                result = result - Union(p[0], p[1], parent, rank);
+            }
+            return result;
+        }
+};
+
 int main() {
-    vector<vector<int>> edges = {{0,1}, {1,2}, {2,3}, {4,5}};
-    int n = 6;
-    Solution sln = Solution();
-    int result = sln.countComponents(n, edges);
-    cout << result << endl;
+    vector<vector<int>> edges = {{0,1}, {1,2}, {2,3}, {4,5}, {6, 7}};
+    int n = 8;
+    DFSApproach* sln_DFS = new DFSApproach();
+    UFApproach* sln_UF = new UFApproach();
+    int result_DFS = sln_DFS->countComponents(n, edges);
+    int result_UF = sln_UF->countComponents(n, edges);
+    cout << "DFS Approach: " << result_DFS << endl;
+    cout << "Union Find Approach: " << result_UF << endl;
 }
